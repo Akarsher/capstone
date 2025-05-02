dea33c4f192158eb97aae1d670f961d9d63f5b9d
Adithya S Nair <adithyasnair25@gmail.com>
2025-05-02 08:52:54 +0530

    error fix 2.0

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -26,10 +26,10 @@ const MobileNav = () => {
         <SheetContent side="left" className="border-none bg-dark-1">
           <Link href="/" className="flex items-center gap-1">
             <Image
-              src="/icons/logo.svg"
+              src="/icons/file.svg"
               width={32}
               height={32}
-              alt="yoom logo"
+              alt="mits logo"
             />
             <p className="text-[26px] font-extrabold text-white">YOOM</p>
           </Link>
